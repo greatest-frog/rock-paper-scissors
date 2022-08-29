ffffa12d44fc38df06271a668067db9856c4d500
greatest-frog <110398760+greatest-frog@users.noreply.github.com>
2022-08-29 16:17:28 +0300

    Add round function

--- resources/js/game.js
+++ resources/js/game.js
@@ -10,3 +10,39 @@ function getComputerChoice() {
         return "Scissors";
     }
 }
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection.toLowerCase() === "rock") {
+        if (computerSelection.toLowerCase() === "rock") {
+            return "Draw. Rock does not beat Rock";
+        }
+        else if (computerSelection.toLowerCase() === "paper") {
+            return "Lose. Paper beats Rock";
+        }
+        else {
+            return "Win. Rock beats Scissors";
+        }
+    }
+    else if (playerSelection.toLowerCase() === "paper") {
+        if (computerSelection.toLowerCase() === "rock") {
+            return "Win. Paper beats Rock";
+        }
+        else if (computerSelection.toLowerCase() === "paper") {
+            return "Draw. Paper does not beat Paper";
+        }
+        else {
+            return "Lose. Scissors beat Paper";
+        }
+    }
+    else {
+        if (computerSelection.toLowerCase() === "rock") {
+            return "Lose. Rock beats Scissors";
+        }
+        else if (computerSelection.toLowerCase() === "paper") {
+            return "Win. Scissors beat Paper";
+        }
+        else {
+            return "Draw. Scissors do not beat Scissors";
+        }
+    }
+}
